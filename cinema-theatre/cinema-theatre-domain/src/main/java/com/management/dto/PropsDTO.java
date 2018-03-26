d839851af8457031dff5b4b8bce5c9886655e2d4
nikola310 <nikola_stojanovic1@yahoo.com>
2018-03-26 21:30:44 +0200

    FanZone and Props validation added.

--- cinema-theatre/cinema-theatre-domain/src/main/java/com/management/dto/PropsDTO.java
+++ cinema-theatre/cinema-theatre-domain/src/main/java/com/management/dto/PropsDTO.java
@@ -2,24 +2,34 @@ package com.management.dto;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * @author Zivko Stanisic
  *
  */
 public class PropsDTO {
-	
+
 	private int propsId;
+	@Size(min = 1, max = 50)
+	@NotNull
 	private String propsName;
+	@NotNull
 	private boolean propsUsed;
 	private char propsType;
+	@NotNull
 	private Date propsDeadline;
+	@NotNull
 	private float propsPrice;
+	
 	private byte[] propsImage;
 	private String propsDesc;
-	
+
 	public int getPropsId() {
 		return propsId;
 	}
+
 	public void setPropsId(int propsId) {
 		this.propsId = propsId;
 	}
@@ -27,42 +37,55 @@ public class PropsDTO {
 	public String getPropsName() {
 		return propsName;
 	}
+
 	public void setPropsName(String propsName) {
 		this.propsName = propsName;
 	}
+
 	public boolean isPropsUsed() {
 		return propsUsed;
 	}
+
 	public void setPropsUsed(boolean propsUsed) {
 		this.propsUsed = propsUsed;
 	}
+
 	public char getPropsType() {
 		return propsType;
 	}
+
 	public void setPropsType(char propsType) {
 		this.propsType = propsType;
 	}
+
 	public Date getPropsDeadline() {
 		return propsDeadline;
 	}
+
 	public void setPropsDeadline(Date propsDeadline) {
 		this.propsDeadline = propsDeadline;
 	}
+
 	public float getPropsPrice() {
 		return propsPrice;
 	}
+
 	public void setPropsPrice(float propsPrice) {
 		this.propsPrice = propsPrice;
 	}
+
 	public byte[] getPropsImage() {
 		return propsImage;
 	}
+
 	public void setPropsImage(byte[] propsImage) {
 		this.propsImage = propsImage;
 	}
+
 	public String getPropsDesc() {
 		return propsDesc;
 	}
+
 	public void setPropsDesc(String propsDesc) {
 		this.propsDesc = propsDesc;
 	}
