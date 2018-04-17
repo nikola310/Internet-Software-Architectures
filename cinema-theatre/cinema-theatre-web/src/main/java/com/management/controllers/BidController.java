50654bb4b2a76b36ad8df4e18b9f623bd0c7acfc
Zivko Stanisic <zivkostanisic@hotmail.com>
2018-04-17 15:09:33 +0200

    User controller tests fixed.

--- cinema-theatre/cinema-theatre-web/src/main/java/com/management/controllers/BidController.java
+++ cinema-theatre/cinema-theatre-web/src/main/java/com/management/controllers/BidController.java
@@ -1,6 +1,5 @@
 package com.management.controllers;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
--- cinema-theatre/cinema-theatre-web/src/test/java/com/management/fake/PerformanceRepositoryFake.java
+++ cinema-theatre/cinema-theatre-web/src/test/java/com/management/fake/PerformanceRepositoryFake.java
@@ -26,6 +26,7 @@ public class PerformanceRepositoryFake implements PerformanceRepository{
 	public void deleteInBatch(Iterable<Performance> arg0) {
 		
 	}
+	
 
 	public List<Performance> findAll() {
 		return null;
